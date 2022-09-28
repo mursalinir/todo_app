35bf062d900d798699ec08dbeff205253b69cf13
Mursalin Islam <mursalinir@gmail.com>
2022-09-29 00:06:27 +0600

    update

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ Things you may want to cover:
 * Database creation
     rails db:create
 * Database initialization
-
+    rails db:migrate
 * How to run the test suite
     rails server
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_24_134137) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_28_180459) do
+  create_table "tasks", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "todo_lists", force: :cascade do |t|
     t.string "title"
     t.string "description"
